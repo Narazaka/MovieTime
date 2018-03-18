563b1de57f4f35eeb1c55bcf44dbd42d4f4cb49c
Narazaka <info@narazaka.net>
2018-03-18 23:32:05 +0900

    rename2

--- MovieTime/ScreenSaverWindow.xaml.cs
+++ MovieTime/ScreenSaverWindow.xaml.cs
@@ -10,15 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MovieTime {
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    /// ScreenSaverWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class ScreenSaverWindow : Window {
+        public ScreenSaverWindow() {
             InitializeComponent();
         }
     }
